1a73441c0a62ec1ba9de620e21d5c853a5de815f
Verevkina Elena <verevkinaee@gmail.com>
2021-12-07 15:48:11 +0200

    add get_hash

--- service/user.py
+++ service/user.py
@@ -1,4 +1,7 @@
+import hashlib
+
 from dao.user import UserDAO
+from constants import PWD_HASH_SALT, PWD_HASH_ITERATIONS
 
 
 class UserService:
@@ -20,3 +23,12 @@ class UserService:
 
     def delete(self, uid):
         self.dao.delete(uid)
+
+    def get_hash(self, password):
+        return hashlib.pbkdf2_hmac(
+            'sha256',
+            password.encode('utf-8'),
+            PWD_HASH_SALT,
+            PWD_HASH_ITERATIONS
+        ).decode("utf-8", "ignore")
+
